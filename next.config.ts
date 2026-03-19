318fb39c83de625c6393c6727b862fe3a0ac7eaa
Paweł Pienta <pawelpienta@Mac-Studio-Pawe.local>
2026-03-19 10:46:51 +0100

    projects wireframe

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
-import type { NextConfig } from 'next';
-
 const nextConfig = {
   reactCompiler: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 } satisfies Record<string, unknown>;
 
 export default nextConfig;
